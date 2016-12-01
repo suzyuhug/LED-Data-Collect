379e4f2fb4f925830d4c6a341d7cba411f0926ac
suzyuhug <Yufei.huang@flextronics.com>
2016-12-01 15:53:39 +0800

    添加项目文件。

---
+++ 端口采集/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace 端口采集
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(449, 531);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(572, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "速度：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10",
+            "20",
+            "30",
+            "40",
+            "50",
+            "60",
+            "70",
+            "100",
+            "200",
+            "300",
+            "400",
+            "500",
+            "600",
+            "700",
+            "800",
+            "900"});
+            this.comboBox1.Location = new System.Drawing.Point(621, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(101, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "200";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(514, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 71);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "打开所有LED灯";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(685, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 71);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "关闭所有LED灯";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(604, 282);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 71);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "采集LED对应的料号";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(564, 386);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 26);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Visible = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(513, 209);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(135, 8);
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(805, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 592);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 端口采集/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 端口采集
+{
+    
+    public partial class Form1 : Form
+    {
+        int idtmp;
+        int hh;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.KeyDown += textbox1_KeyDown;
+
+           
+           
+        }
+
+        private void textbox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode  == Keys.Enter)
+            {
+                label2.Text = "36665";
+                insertpn();
+                updatapn();
+                textBox1.Clear();
+                textBox1.Focus();
+
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+
+        {
+
+    
+            try
+            {
+            string SqlData = "server=10.194.48.150\\MySQL;database=KTE;uid=sa;pwd=Aa123456";
+            SqlConnection cn = new SqlConnection(SqlData );
+            SqlCommand cmd = new SqlCommand("protview", cn);                          
+            SqlDataAdapter dp = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            dp.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+                cn.Close();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            progressBar1.Maximum = dataGridView1.RowCount - 1;
+            progressBar1.Left = button1.Left - 1;
+            progressBar1.Visible = true;
+            button1.Enabled = false;
+            for (int i = 0; i < dataGridView1.RowCount-1; i++)
+            {
+                Thread.Sleep(int.Parse(comboBox1.Text));
+                progressBar1.Value = i;
+
+                string ipval;
+                int protval;
+                string send;
+                ipval = dataGridView1.Rows[i].Cells["Ip"].Value.ToString();
+                protval  = int.Parse(dataGridView1.Rows[i].Cells["port"].Value.ToString());
+                send  = dataGridView1.Rows[i].Cells["send"].Value.ToString()+"_0";
+
+                sendmessage(ipval, protval, send);
+
+                label2.Text = send ;
+
+
+
+
+            }
+            progressBar1.Value = 0;
+            progressBar1.Visible = false;
+            button1.Enabled = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = dataGridView1.RowCount - 1;
+            progressBar1.Left = button2.Left - 1;
+            progressBar1.Visible = true;
+            button2.Enabled = false;
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                Thread.Sleep(int.Parse(comboBox1.Text));
+                progressBar1.Value = i;
+
+                string ipval;
+                int protval;
+                string send;
+                ipval = dataGridView1.Rows[i].Cells["Ip"].Value.ToString();
+                protval = int.Parse(dataGridView1.Rows[i].Cells["port"].Value.ToString());
+                send = dataGridView1.Rows[i].Cells["send"].Value.ToString() + "_1";
+
+                sendmessage(ipval, protval, send);
+            }
+            progressBar1.Value = 0;
+            progressBar1.Visible = false;
+            button2.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            hh = 0;
+            textBox1.Visible = true;
+            insertpn();
+
+        }
+
+        public void insertpn()
+        {
+            string ip;
+            int prot;
+            string send;
+            int i;
+
+            if (hh > 0)
+            {
+                i = hh-1;
+                ip = dataGridView1.Rows[i].Cells["Ip"].Value.ToString();
+                prot = int.Parse(dataGridView1.Rows[i].Cells["port"].Value.ToString());
+                send = dataGridView1.Rows[i].Cells["send"].Value.ToString() + "_1";
+                idtmp = int.Parse(dataGridView1.Rows[i].Cells["ID"].Value.ToString());
+                //  关闭LED
+                Thread.Sleep(int.Parse(comboBox1.Text));
+                sendmessage(ip,prot,send);
+                
+
+            }
+
+            i =hh;
+            ip= dataGridView1.Rows[i].Cells["Ip"].Value.ToString();
+            prot= int.Parse (dataGridView1.Rows[i].Cells["port"].Value.ToString());
+            send = dataGridView1.Rows[i].Cells["send"].Value.ToString() + "_0";
+            Thread.Sleep(int.Parse(comboBox1.Text));
+            //打开LED
+            sendmessage(ip, prot, send);
+            hh++;
+            label2.Text = hh.ToString();
+
+
+
+        }
+
+
+        public void updatapn()
+        {
+
+            try
+            {
+                
+                string SqlData = "server=10.194.48.150\\MySQL;database=KTE;uid=sa;pwd=Aa123456";
+                SqlConnection cn = new SqlConnection(SqlData);
+                string ii = "exec updatapn '" + idtmp +"','"+ textBox1.Text + "'";
+                SqlCommand cmd = new SqlCommand(ii, cn);             
+                cn.Open();
+                cmd.ExecuteNonQuery();              
+                cn.Close();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            updatapn();
+
+        }
+
+        public void sendmessage(string ip, int prot, string message)
+        {
+
+
+            try
+            {
+
+                TcpClient tcpClient = new TcpClient();
+                tcpClient.Connect(IPAddress.Parse(ip), prot);
+                NetworkStream ns = tcpClient.GetStream();
+                byte[] bytes = new byte[1024];
+                bytes = System.Text.Encoding.ASCII.GetBytes(message);
+            try
+            {
+                ns.Write(bytes, 0, bytes.Length);
+            }
+            catch
+            {
+                Console.WriteLine("missing");
+
+            }
+
+            ns.Close();
+            tcpClient.Close();
+
+            }
+            catch (Exception)
+            {
+
+              
+            }
+
+           
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
